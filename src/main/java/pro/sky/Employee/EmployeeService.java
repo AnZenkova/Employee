141e98b32b30e34d2fd2bdc0be7de3db18959553
AnZenkova <nastyonka16@mail.ru>
2022-02-24 17:36:25 +1000

    Step 4

---
+++ src/main/java/pro/sky/Employee/EmployeeService.java
@@ -0,0 +1,17 @@
+package pro.sky.Employee;
+
+public class EmployeeService {
+    Employee[] employees = {
+            new Employee("Крылов", "Тихон"),
+            new Employee("Маслова", "Милиса"),
+            new Employee("Яковлев", "Леонид"),
+            new Employee("Полякова", "Божена"),
+            new Employee("Волков", "Мирослав"),
+            new Employee("Анисимова", "Данна"),
+            new Employee("Соловьёв", "Ефрем"),
+            new Employee("Меркушева", "Глория"),
+            new Employee("Никитин", "Адам"),
+            new Employee("Кудрявцева", "Галина"),
+
+    };
+}
