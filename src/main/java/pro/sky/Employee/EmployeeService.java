c14cc09d0a385456797a86f46086a619af32c79a
AnZenkova <nastyonka16@mail.ru>
2022-02-24 17:50:10 +1000

    Step 5

--- src/main/java/pro/sky/Employee/EmployeeService.java
+++ src/main/java/pro/sky/Employee/EmployeeService.java
@@ -12,6 +12,32 @@ public class EmployeeService {
             new Employee("Меркушева", "Глория"),
             new Employee("Никитин", "Адам"),
             new Employee("Кудрявцева", "Галина"),
-
     };
+
+    public void addNewEmployee(String employeeLastName, String employeeName) {
+        for (int i = 0; i < employees.length; i++) {
+            if (employees[i] == null) {
+                employees[i] = new Employee(employeeLastName, employeeName);
+                break;
+            }
+        }
+    }
+
+    public void removeEmployee(String employeeLastName, String employeeName) {
+        for (int i = 0; i < employees.length; i++) {
+            if (employees[i].getEmployeeLastName().equals(employeeLastName) && employees[i].getEmployeeName().equals(employeeName)) {
+                employees[i] = null;
+            }
+        }
+    }
+
+    public String findEmployees(String employeeLastName, String employeeName) {
+        String employee = null;
+        for (int i = 0; i < employees.length; i++) {
+            if (employees[i].getEmployeeLastName().equals(employeeLastName) && employees[i].getEmployeeName().equals(employeeName)) {
+                    employee = employees[i].getEmployeeLastName() + " " + employees[i].getEmployeeName() + ";";
+            }
+        }
+        return employee;
+    }
 }
