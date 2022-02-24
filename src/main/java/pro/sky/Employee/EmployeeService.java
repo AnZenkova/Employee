bec028dbd02e3e65d0532463f3cdc21b5ec43532
AnZenkova <nastyonka16@mail.ru>
2022-02-24 20:39:54 +1000

    Выполнила

--- src/main/java/pro/sky/Employee/EmployeeService.java
+++ src/main/java/pro/sky/Employee/EmployeeService.java
@@ -1,7 +1,11 @@
 package pro.sky.Employee;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class EmployeeService {
-    Employee[] employees = {
+    private int size;
+    public Employee[] employees = {
             new Employee("Крылов", "Тихон"),
             new Employee("Маслова", "Милиса"),
             new Employee("Яковлев", "Леонид"),
@@ -14,29 +18,51 @@ public class EmployeeService {
             new Employee("Кудрявцева", "Галина"),
     };
 
-    public void addNewEmployee(String employeeLastName, String employeeName) {
+    public void addNewEmployee(String employeeName, String employeeLastName) {
+        if (size >= employees.length) {
+            throw new ArrayIsFull();
+        }
         for (int i = 0; i < employees.length; i++) {
+            if (employees[i].getEmployeeLastName().equals(employeeLastName) && employees[i].getEmployeeName().equals(employeeName)) {
+                throw new EmployeeIsInArray();
+            }
             if (employees[i] == null) {
                 employees[i] = new Employee(employeeLastName, employeeName);
                 break;
+            } else {
+                size++;
             }
         }
     }
 
-    public void removeEmployee(String employeeLastName, String employeeName) {
+    public void removeEmployee(String employeeName, String employeeLastName) {
+        int a = 0;
         for (int i = 0; i < employees.length; i++) {
             if (employees[i].getEmployeeLastName().equals(employeeLastName) && employees[i].getEmployeeName().equals(employeeName)) {
                 employees[i] = null;
             }
+            if (employees[i] != null) {
+                a++;
+            }
+        }
+        if (a == employees.length) {
+            throw new NotFound();
         }
     }
 
-    public String findEmployees(String employeeLastName, String employeeName) {
+    public String findEmployees(String employeeName, String employeeLastName) {
+        int a = 0;
         String employee = null;
         for (int i = 0; i < employees.length; i++) {
             if (employees[i].getEmployeeLastName().equals(employeeLastName) && employees[i].getEmployeeName().equals(employeeName)) {
                     employee = employees[i].getEmployeeLastName() + " " + employees[i].getEmployeeName() + ";";
             }
+            if (employees[i] == null) {
+                a++;
+            }
+        }
+        if (a == size) {
+            throw new NotFound();
         }
         return employee;
     }
